4c8a824ead8cf0e3afd8230758407df0406ab040
Dominik Wache <dominik.wache@web.de>
2022-05-20 12:02:44 +0200

    Ready

--- js/utils.js
+++ js/utils.js
@@ -1,36 +1,37 @@
-//attack collision detection => return true or false
-function collisionDetected({ rectangle1, rectangle2 }) {
+function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
-    rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-    rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+      rectangle2.position.x &&
+    rectangle1.attackBox.position.x <=
+      rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+      rectangle2.position.y &&
     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  );
+  )
 }
 
-//winner determination
 function determineWinner({ player, enemy, timerId }) {
-  clearTimeout(timerId);
-  displayNotification.style.display = 'flex';
+  clearTimeout(timerId)
+  document.querySelector('#displayText').style.display = 'flex'
   if (player.health === enemy.health) {
-    displayNotification.innerHTML = 'Tie';
+    document.querySelector('#displayText').innerHTML = 'Tie'
   } else if (player.health > enemy.health) {
-    displayNotification.innerHTML = 'Player Won';
+    document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
   } else if (player.health < enemy.health) {
-    displayNotification.innerHTML = 'Enemy Won';
+    document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
   }
 }
 
-//timer
-let timer = 60;
-let timerId;
+let timer = 60
+let timerId
 function decreaseTimer() {
   if (timer > 0) {
-    timerId = setTimeout(decreaseTimer, 1000);
-    document.querySelector('.timer').innerHTML = --timer;
+    timerId = setTimeout(decreaseTimer, 1000)
+    timer--
+    document.querySelector('#timer').innerHTML = timer
   }
 
   if (timer === 0) {
-    determineWinner({ player, enemy, timerId });
+    determineWinner({ player, enemy, timerId })
   }
 }
